983f3b84c3c406b766669ba1e0f00e87a62f3d34
Hartmann <hartmann_psi@sjtu.edu.cn>
2024-08-13 17:29:16 +0800

    frame:移动main.java在文件夹中的相对位置

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 rm -r antlr4/.antlr/
-find antlr4 -name '*.java' | xargs javac -d bin -cp lib/antlr-4.13.2-complete.jar
+find ./ -name '*.java' | xargs javac -d bin -cp lib/antlr-4.13.2-complete.jar
